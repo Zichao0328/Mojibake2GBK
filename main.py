ff8ff879fae2feb8e12c5ca23c2943b5c0f3490f
Zichao0328 <zpalqmwerxcv@gmail.com>
2023-10-24 22:44:09 +0800

    去除冗余注释

--- main.py
+++ main.py
@@ -7,26 +7,6 @@ from Renamer import Renamer
 VERSION = '0.1.0'
 
 
-
-# def rename_files(path):
-#     file_abs_path = []
-#     '''
-#     自底向上遍历，防止更改父文件夹名后找不到子文件夹/子文件
-#     '''
-#     for root, dirs, files in os.walk(path, topdown=False):
-#         for name in files + dirs:
-#             old_path = os.path.join(root, name)
-#             file_abs_path.append(old_path)
-#     '''
-#     修改文件名
-#     '''
-#     for abs_path in file_abs_path:
-#         old_path, old_name = os.path.split(abs_path)
-#         new_name = old_name.encode('gbk').decode('shift_jis')
-#         os.rename(abs_path, os.path.join(old_path, new_name))
-#     return file_abs_path
-
-
 class AppFrame(MainFrame):
 
     def on_dir_changed_event(self, event):
@@ -55,12 +35,6 @@ class AppFrame(MainFrame):
 
 if __name__ == '__main__':
     gbk_string = '01_wav（効果音あり）'
-    # patten = "^[u4E00-u9FFF]+"
-    # print(re.match(patten, gbk_string))
-    # shift_jis_string = gbk_string.encode('gbk').decode('shift_jis')
-    # print(shift_jis_string)
-    path = 'D:\埑敆鋜傔-僀僠儍儔僽媡栭攪偄曇'
-    # file_names = rename_files(path)
     app = wx.App(False)
     nwid = AppFrame(None)
     nwid.SetTitle(f'DLSite 同人作品日文乱码转换工具 v{VERSION}')
